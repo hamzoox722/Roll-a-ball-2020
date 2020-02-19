f5386105e93a3413625c4c5299550a3c6889d611
hamzoox722 <60244310+hamzoox722@users.noreply.github.com>
2020-02-19 17:48:33 -0500

    Commit 6
    Still Trying

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,15 +10,14 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 pos = transform.position;
-        pos.x += speed * Time.deltaTime;
-        transform.position = pos;
-
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position += transform.forward * speed * Time.deltaTime;
         if (transform.position.x> rightX)
         {
             Destroy(this.gameObject);
---
+++ Assets/Scripts/BulletSource.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSource : MonoBehaviour
+{
+    public float fireRate = 2f;
+    public float chanceToChangeDirections = 0.1f;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Fire", 2f, fireRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+
+    void Fire()
+    {
+        GameObject Bullet = Instantiate(BulletPrefab) as GameObject;
+        Bullet.transform.position = transform.position;
+
+    }
+}
+}
