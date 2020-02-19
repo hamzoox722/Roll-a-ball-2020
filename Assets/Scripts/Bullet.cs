22910ffa346ea579fca663bd95f15c0c25ac8d85
hamzoox722 <60244310+hamzoox722@users.noreply.github.com>
2020-02-19 17:32:06 -0500

    commit5
    another attempt at projectile physics

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,10 +6,14 @@ public class Bullet : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public static float rightX = 30f;
+    public float speed = 30;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
     }
 
     // Update is called once per frame
